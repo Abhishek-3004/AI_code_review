bdacb21a94d8b9bf9aeacd23ad138f46fa529894
Abhishek <abhishek120901@gmail.com>
2025-05-05 12:49:45 +0530

    new file factorial

---
+++ factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int factorial(int n);
+int main()
+{
+    int n;
+    cout << "Enter any number to find its factorial." << endl;
+    cin >> n;
+    cout << "Factorial of entered number is " << factorial(n);
+
+    return 0;
+}
+int factorial(int n)
+{
+    if (n < 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
